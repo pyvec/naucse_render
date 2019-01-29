62094288f3d8775829f97eb1063c4c76b5ea92e6
Petr Viktorin <encukou@gmail.com>
2019-01-29 17:16:39 +0100

    read_yaml: invalidate cache when file changes, and add tests

--- naucse_render/load.py
+++ naucse_render/load.py
@@ -6,20 +6,31 @@ import yaml
 
 
 @functools.lru_cache()
-def _read_yaml(path):
+def _read_yaml(path, stat):
     print('Loading', path, file=sys.stderr)
     with path.open(encoding='utf-8') as f:
         return yaml.safe_load(f)
 
 
 def read_yaml(base_path, *path_parts, source_key=None):
+    """Read the given YAML file
+
+    Since YAML reading is an expensive operation, the results are cached
+    based on filename and stat info (size, modification time, inode number
+    etc.)
+
+    The base_path and path_parts are joined using Path.joinpath, but
+    the file may not live outside base_path (e.g. '../foo.yaml' isn't allowed.)
+    The base_bath should be a directory.
+    """
+
     yaml_path = base_path.joinpath(*path_parts).resolve()
 
     # Guard against '..' in the course_slug
     if base_path not in yaml_path.parents:
         raise ValueError(f'Invalid path')
 
-    result = dict(_read_yaml(yaml_path))
+    result = dict(_read_yaml(yaml_path, yaml_path.stat()))
     if source_key:
         result[source_key] = '/'.join(path_parts)
     return result
---
+++ test_naucse_render/test_load.py
@@ -0,0 +1,59 @@
+import pytest
+
+from naucse_render.load import read_yaml, _read_yaml
+
+
+def test_read_yaml(tmp_path):
+    yaml_path = tmp_path / 'test.yaml'
+    yaml_path.write_text("""data:
+        - 1
+        - 2
+        - 3
+    """)
+    assert read_yaml(tmp_path, 'test.yaml') == {'data': [1, 2, 3]}
+
+
+def test_read_yaml_uses_cache(tmp_path):
+    """Assert that read_yaml uses a cache"""
+    yaml_path = tmp_path / 'test.yaml'
+    yaml_path.write_text("""data:
+        - 1
+        - 2
+        - 3
+    """)
+
+    # We assert that cache is used by looking at lru_cache information from
+    # the internal function.
+    start_info = _read_yaml.cache_info()
+
+    first = read_yaml(tmp_path, 'test.yaml')
+
+    info = _read_yaml.cache_info()
+    assert info.hits == start_info.hits
+    assert info.misses == start_info.misses + 1
+
+    second = read_yaml(tmp_path, 'test.yaml')
+    info = _read_yaml.cache_info()
+    assert info.hits == start_info.hits + 1
+    assert info.misses == start_info.misses + 1
+
+    assert first == second
+
+
+def test_read_yaml_break_cache(tmp_path):
+    """Assert that read_yaml cache is invalidated when the file changes"""
+    yaml_path = tmp_path / 'test.yaml'
+    yaml_path.write_text("""data:
+        - 1
+        - 2
+        - 3
+    """)
+    assert read_yaml(tmp_path, 'test.yaml') == {'data': [1, 2, 3]}
+    yaml_path.write_text("changed: 1")
+    assert read_yaml(tmp_path, 'test.yaml') == {'changed': 1}
+
+
+def test_read_yaml_disallow_parents(tmp_path):
+    """Assert that read_yaml cache is invalidated when the file changes"""
+    with pytest.raises(ValueError):
+        read_yaml(tmp_path, '../../test.yaml')
