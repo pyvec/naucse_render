2239d226f13dfc45fa67ce05026c0b3e5d78f82c
Petr Viktorin <encukou@gmail.com>
2022-02-15 14:38:54 +0100

    Allow compiling all defined courses (#30)

--- naucse_render/cli.py
+++ naucse_render/cli.py
@@ -10,7 +10,7 @@ def main():
     pass
 
 @main.command()
-@click.argument('destination', metavar='DIR')
+@click.argument('destination', metavar='DIR', type=Path)
 @click.option(
     '--slug', default=None,
     help='Slug of the course to compile')
@@ -23,21 +23,52 @@ def main():
 @click.option(
     '--edit-repo-branch',
     help='Branch in the repository where the content can be edited')
-def compile(slug, path, destination, edit_repo_url, edit_repo_branch):
+@click.option(
+    '--all/--no-all', 'compile_all', default=None,
+    help='Compile all available courses. By default, this is done if '
+    + "a default course isn't found")
+def compile(
+    slug, path, destination, edit_repo_url, edit_repo_branch, compile_all,
+):
     """Compile the given course to a directory with JSON & HTML data"""
     edit_info = {}
     if edit_repo_url:
         edit_info['url'] = edit_repo_url
     if edit_repo_branch:
         edit_info['branch'] = edit_repo_branch
+    if compile_all is None:
+        if None in naucse_render.get_course_slugs(path=path):
+            compile_all = False
+        else:
+            compile_all = not slug
+    if compile_all and slug is not None:
+        click.fail('Cannot use --all with --slug.')
     if slug == '':
         slug = None
-    naucse_render.compile(
-        slug=slug,
-        path=path,
-        destination=destination,
-        edit_info=edit_info,
-    )
+    if compile_all:
+        for slug in naucse_render.get_course_slugs(path=path):
+            if slug is None:
+                click.fail('Cannot use --all with a default course.')
+            slug = removeprefix(slug, 'courses/')
+            naucse_render.compile(
+                slug=slug,
+                path=path,
+                destination=destination / slug,
+                edit_info=edit_info,
+            )
+    else:
+        naucse_render.compile(
+            slug=slug,
+            path=path,
+            destination=destination,
+            edit_info=edit_info,
+        )
+
+def removeprefix(string, prefix):
+    """str.removeprefix(). Remove when support for Python 3.8 is droped."""
+    if string.startswith(prefix):
+        return string[len(prefix):]
+    return string
 
 @main.command()
 @click.argument('slug', metavar='SLUG', default=None)
